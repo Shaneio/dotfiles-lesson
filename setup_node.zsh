631f57b85727e7a01334aa444f3615150e3e7365
Shaneio <44534398+Shaneio@users.noreply.github.com>
2022-07-01 13:04:37 +0100

    Add useful global npm packages

--- setup_node.zsh
+++ setup_node.zsh
@@ -17,3 +17,16 @@ else
  n latest
  n lts
 fi
+
+# Install Global NPM Packages
+
+npm install --global firebase-tools
+npm install --global @angular/cli
+npm install --global @ionic/cli
+npm install --global typescript
+npm install --global json-server
+npm install --global http-server
+npm install --global chalk
+
+echo "Global NPM Packages Installed:"
+npm list --global --depth=0
